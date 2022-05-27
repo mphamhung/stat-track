a8ed5957ef66d4dab1092102b49484c120a21c99
Michael <mphamhung@gmail.com>
2022-05-27 13:05:44 -0400

    added button groups

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 const plays = []
@@ -11,6 +12,9 @@ function buttonClicked(name) {
   if (passList.length == 0 && (name.member == 'Goal' || name.member == 'Throwaway' )) {
   
   }
+  if (passList.length > 0 && (name.member == 'D')) {
+  
+  }
   else if (passList[passList.length - 1] != name.member) {
   passList.push(name.member)
   }
@@ -19,7 +23,7 @@ function buttonClicked(name) {
   if (passList[passList.length - 1] == 'Goal' || name.member == 'Throwaway' ){
     plays.push(passList)
     console.log(plays)
-
+    alert(plays)
     passList = []
   }
   else {
@@ -30,19 +34,26 @@ function buttonClicked(name) {
 }
 
 
-const members = ['Michael', 'Marco', 'Nancy', 'Goal', 'Throwaway', "D"];
-
+const members = ['Michael', 'Marco', 'Nancy'];
+const actions = ['Goal', 'Throwaway', "D"];
 const buttonMembers = members.map((member) =>
   <Button onClick={() => buttonClicked({member})}>{member}</Button>
+)
+
+const buttonActions = actions.map((member) =>
+<Button onClick={() => buttonClicked({member})}>{member}</Button>
 )
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <ButtonGroup orientation="vertical" size='medium' variant="contained" aria-label="outlined primary button group">
         {buttonMembers}
-
+        </ButtonGroup>
+        <ButtonGroup size="large" aria-label="text button group">
+        {buttonActions}
+        </ButtonGroup>
 
       </header>
     </div>
