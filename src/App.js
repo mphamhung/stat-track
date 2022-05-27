bceeb973e507b23c934d6abbb8973b364c3d922d
Michael <mphamhung@gmail.com>
2022-05-27 12:52:08 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+
+
+const plays = []
+var passList = []
+
+function buttonClicked(name) {
+  // alert('Hello', {name})
+  if (passList.length == 0 && (name.member == 'Goal' || name.member == 'Throwaway' )) {
+  
+  }
+  else if (passList[passList.length - 1] != name.member) {
+  passList.push(name.member)
+  }
+
+  
+  if (passList[passList.length - 1] == 'Goal' || name.member == 'Throwaway' ){
+    plays.push(passList)
+    console.log(plays)
+
+    passList = []
+  }
+  else {
+    console.log(passList)
+  }
+  
+  // passList.append()
+}
+
+
+const members = ['Michael', 'Marco', 'Nancy', 'Goal', 'Throwaway', "D"];
+
+const buttonMembers = members.map((member) =>
+  <Button onClick={() => buttonClicked({member})}>{member}</Button>
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {buttonMembers}
+
+
       </header>
     </div>
   );
