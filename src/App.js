d045efdfe4c0009a33b9f36000a8366aa5edd978
Michael <mphamhung@gmail.com>
2022-05-28 12:43:39 -0400

    post del players

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Alert, Button, ButtonGroup, Container} from '@mui/material';
+import {Alert, Button, ButtonGroup, Container, FormControl, TextField} from '@mui/material';
 // import ButtonGroup from '@mui/material/ButtonGroup';
 // import Alert from '@mui/material/Alert';
 import React from 'react'
@@ -20,20 +20,107 @@ function DisplayPlay(props) {
   }
 }
 
+
+const NewPlayer = () => {
+  const [name, setName] = React.useState('')
+  const [gender, setGender] = React.useState('M')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();    
+    const newplayer = {name, gender}
+
+    if (e.nativeEvent.submitter.name == 'del') {
+      // console.log("https://stat-track-db.herokuapp.com/players?name="+name)
+      fetch("https://stat-track-db.herokuapp.com/players/"+name, {
+      method: 'DELETE',
+    }).then(() => {
+      console.log('player deleted')
+    })
+    }
+
+    else if (e.nativeEvent.submitter.name == 'new') {
+      fetch("https://stat-track-db.herokuapp.com/players", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(newplayer)
+      }).then(() => {
+        console.log('new player added')
+      })
+    }
+    else  {
+  
+  }
+
+  }
+
+  return (
+    <div>
+      <form
+      onSubmit={handleSubmit}>
+        <input
+          type = 'text'
+          value={name}
+          onChange = {(e)=> setName(e.target.value)}
+        />
+        <select
+        value = {gender}
+        onChange ={(e) => setGender(e.target.value)}>
+          <option value = 'M'>M</option>
+          <option value = 'F'>F</option>
+        </select>
+        <button name='new'>New Player</button>
+        <button name='del'>Delete Player</button>
+      </form>
+    </div>
+    )
+  }
+
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       passes : [],
       play: [],
+      posts: [],
+      males: [],
+      females: []
     }
   }
 
+  componentDidMount(){
+
+    fetch("https://stat-track-db.herokuapp.com/players?gender=M")
+    .then(resp => resp.json())
+    .then (data => {
+      let players = data.map((player) => {
+        return (player.name)
+      }
+      )
+      this.setState({males:players})
+    })
+    fetch("https://stat-track-db.herokuapp.com/players?gender=F")
+    .then(resp => resp.json())
+    .then (data => {
+      let players = data.map((player) => {
+        return (player.name)
+      }
+      )
+      this.setState({females:players})
+    })
+  }
 
   renderPlays() {
     return this.state.play.map((member) =>
     <DisplayPlay play={member}>{member}</DisplayPlay>
    )
+  }
+
+  handlePlayerClick(props) {
+    if (passList[passList.length - 1] !== props.member) {
+      passList.push(props.member)
+      }
   }
   buttonClicked(name) {
     // alert('Hello', {name})
@@ -64,12 +151,15 @@ class App extends React.Component {
   }
 
 
+
   render() {
-    const members = ['Michael', 'Marco', 'Nancy'];
     const actions = ['Goal', 'Throwaway', "D"];
   
-    const buttonMembers = members.map((member) =>
-      <Button onClick={() => this.buttonClicked({member})}>{member}</Button>
+    const femaleMembers = this.state.females.map((member) =>
+      <Button onClick={() => this.buttonClicked({member})} variant="contained">{member}</Button>
+    )
+    const maleMembers = this.state.males.map((member) =>
+      <Button onClick={() => this.buttonClicked({member})} variant="contained">{member}</Button>
     )
     
     const buttonActions = actions.map((member) =>
@@ -80,12 +170,18 @@ class App extends React.Component {
  
   return (
     <div className="App">
+      <NewPlayer></NewPlayer>
       <Container>
       <ButtonGroup size="large" aria-label="text button group">
         {buttonActions}
       </ButtonGroup>
+      </Container>
+      <Container>
       <ButtonGroup orientation="vertical" size='medium' variant="contained" aria-label="outlined primary button group">
-        {buttonMembers}
+        {femaleMembers}
+      </ButtonGroup>
+      <ButtonGroup orientation="vertical" size='medium' variant="contained" aria-label="outlined primary button group">
+        {maleMembers}
       </ButtonGroup>
       </Container>
       <Container>
