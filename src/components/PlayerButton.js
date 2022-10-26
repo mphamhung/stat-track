7d49daba2946e37eabf4a256900c2172904bc3c0
Michael <mphamhung@gmail.com>
2022-10-25 22:26:37 -0400

    on touch shake

--- src/components/PlayerButton.js
+++ src/components/PlayerButton.js
@@ -5,7 +5,21 @@ import {Button, Box} from '@mui/material';
 import React from 'react'
 
 const padding = 0.2
+const buttonJSS = {
+  "&.active": {
+      animation: "MoveUpDown 0.1s linear",
+      animationFillMode: "forwards"
+  },
 
+  "@keyframes MoveUpDown": {
+      "0%, 100%": {
+          transform: "translateY(0)"
+      },
+      "50%": {
+          transform: "translateY(5px)"
+      }
+  }
+};
 function PlayerButton(props) {
     let name = props.player.name
     let func = props.onClick
@@ -14,14 +28,22 @@ function PlayerButton(props) {
     let isDisabled = props.isDisabled
     let color = ((props.player.gender === "M" ) ? 'primary' : 'secondary')
     let gender = props.player.gender
+    const [active, setActive] = React.useState(false);
 
     function handleOnClick(){
       func({name, id, gender})
+      
     }
 
-
+    function handleOnTouchStart(){
+      setActive(true);
+      setTimeout(() => {
+          setActive(false);
+      }, 300)
+    }
     const handlers={
       onClick: handleOnClick,
+      onTouchStart: handleOnTouchStart,
       // onMouseDown: handleOnMouseDown,
       // onMouseUp: handleOnMouseUp,
   }
@@ -34,8 +56,9 @@ function PlayerButton(props) {
                   name={id} 
                   key={id}
                   disabled={isDisabled}
-                  // onClick={()=>handleOnClick()}
                   {... handlers}
+                  sx={buttonJSS}
+                  className={active ? 'active' : ''}
                   >
                   {name}
             </Button>
