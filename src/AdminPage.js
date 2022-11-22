070b71cf5654bfca4c689cf37363c39256ec535a
Michael <mphamhung@gmail.com>
2022-11-22 14:17:48 -0500

    admin delete

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
-import {ListItemButton} from '@mui/material';
 import { Container } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import {TextField, Button, Typography ,Stack} from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Dialog from '@mui/material/Dialog';
 
 const db_url = "https://polydactyl-truthful-hyena.glitch.me"
 
@@ -9,28 +11,68 @@ const db_url = "https://polydactyl-truthful-hyena.glitch.me"
 
     const [hasFetched, setHasFetched] = useState(false)
     const [adminGames, setAdminGames] = useState([])
+    const navigate  = useNavigate ();
+    const [showDelPrompt, setShowDelPrompt] = useState(false)
+    const [selectedGameInfo, setSelectedGameInfo] = useState(null)
+    const [delPromptVal, setDelPromptVal] = useState('')
+    const handleDelClick = (props) => {
+        console.log(props)
+        console.log(adminGames)
+        console.log('deleted game with id {selectedGameInfo.id}')
+        console.log(delPromptVal)
+        if (delPromptVal === 'delete game '+ selectedGameInfo.id) {
+            fetch(db_url+"/games/"+selectedGameInfo.id, {
+                method: 'DELETE',
+              }).then(resp => {
+                    if (resp.ok) {
+                        let adminList =  adminGames.filter((game)=> game.id !== selectedGameInfo.id)
+                        setAdminGames(adminList)
+                    }     
+                  }
+              )
+            setShowDelPrompt(false)
 
+        }
+        
+      }
     useEffect( () => {
-        fetchGames()
-    }, [])
-
-    function fetchGames() {
-        return fetch(db_url+"/games")
+        fetch(db_url+"/games")
         .then(resp => resp.json())
         .then( data => {  
-            let adminList  = data.filter(game => game.versus === 'default').map(game => {
+            let i =0 
+            let adminList  = data.reverse().map(game => {
                 let query = "?home="+game.team_name+"&versus="+game.versus+"&date="+game.date+"&uID="+game.uID
-                
-                    return <ListItemButton key={"?home="+game.team_name+"&versus="+game.versus+"&date="+game.date+"&uID="+game.uID}>
-                    <Link to={{
-                    pathname:"/summary/"+query,
-                    }}
-                    >
-                    {game.team_name}  versus {game.versus}  game {game.uID} on {game.date}</Link>
-                    </ListItemButton>
+                i += 1
+                    let html = (
+                        <Stack direction='row' 
+                        justifyItems='space-between' 
+                        style={{backgroundColor: i%2 ? '#EEEEEE': 'white',
+                        maxHeight:"50px", minHeight:"50px"}}
+                        key={game.id}
+                        >
+         
+                            <Typography style={{maxWidth:"80px", minWidth:"80px", maxHeight:"60px", minHeight:"60px",
+                                }}>
+                            {game.date.split("-")[0]} {game.date.split("-")[1]}
+                            </Typography>
+                            <Stack direction="column" style={{maxWidth:"60%", minWidth:"60%", maxHeight:"60px", minHeight:"60px",
+                                textDecoration: "underline"}}
+                                onClick={( ) => navigate('/summary/'+query)}>
+                                <Typography>
+                                    {game.team_name}
+                                </Typography>
+                                <Typography>
+                                    {game.versus}  
+                                </Typography>
+                            </Stack>
+                            <DeleteForeverIcon
+                            onClick={() => {setShowDelPrompt(true); setSelectedGameInfo({'id': game.id, 'home':game.team_name, 'away':game.versus})}}
+                            />
+                            
+                        </Stack>
+                    )
+                    return {html, 'id': game.id, 'home':game.team_name, 'away':game.versus}
             })
-            
-            // console.log(gameList)
             setAdminGames(adminList)
     
             setHasFetched(true)
@@ -38,11 +80,36 @@ const db_url = "https://polydactyl-truthful-hyena.glitch.me"
         }
     
         )
-      }
+    }, [navigate])
+
+    // function fetchGames() {
+        
+    //   }
       return(
         <Container>
+             <h4>Viewing All Previous Games</h4>
+            {hasFetched && adminGames.map((game)=> game.html)}
+            {showDelPrompt && 
+            <Dialog
+            open={showDelPrompt}
+            onClose={() => setShowDelPrompt(false)}
+          >
+            
+            <Typography>
+               <h1>
+               Are you sure you want to delete Game {selectedGameInfo.home} vs  {selectedGameInfo.away} with ID {selectedGameInfo.id}? Type "delete game {selectedGameInfo.id}" and confirm if so.
+                </h1> 
+            </Typography>
+            <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 4 }}>
+            <TextField id="outlined-basic" variant="outlined" value={delPromptVal} onChange={(e)=> setDelPromptVal(e.target.value)}/>
 
-        {hasFetched && adminGames}
+            <Button onClick={handleDelClick}>
+                Yes
+            </Button>
+            </Stack>
+            
+          </Dialog>
+          }
         </Container>
       )
  }
